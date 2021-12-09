88dd98852c4910aa56a90fb6d9c8d24171c46ebc
fleuren <91404708+fleuren@users.noreply.github.com>
2021-12-09 18:33:25 +0400

    Add files via upload

---
+++ exer8.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+//Fuel Me Up, Scotty
+/*
+int needGas;
+char gasType;
+int gasLiters;
+double petrolPrice = 4.20;
+double dieselPrice = 6.90;
+
+cout << "Do you need gas? 1 = 'yes', 2 = 'no' " << endl;
+cin >> needGas;
+
+if (needGas == 0){
+	cout << "You may leave now";
+}else{
+cout << "Please input the gas type" << endl;
+cout << "P - petrol, D - diesel" << endl;
+cin >> gasType;
+cout << "Please input the amount of liters desired" << endl;
+cin >> gasLiters;
+switch (gasType) {
+	case 'P':
+	case 'p':
+cout << "You have chosen petrol, and " << gasLiters << " liters of it. The price would be " << gasLiters*petrolPrice << endl;
+break;
+
+	case 'd':
+	case 'D':
+cout << "You have chosen diesel, and " << gasLiters << " liters of it The price would be " << gasLiters*dieselPrice << endl;
+break;
+}	
+}
+
+
+//f to c temp
+	/*
+cout << "Convert 50 to Celsius" << endl;
+cout << "50" << endl;
+int x = 50;
+int y = (x-32) * 5/9;
+cout << y;
+*/
+
+return 0;
+}
+
+
+
+
+
+
+
+
---
+++ lec8p2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	/*
+	//temperature converter
+	int t; //temp variable
+cout << "Please input a temperature" << endl;
+	cin >> t; //input temp
+cout << "The temperature is " << t << endl;
+cout << "Press 'c' to convert this from Fahrenheit to Celsius" << endl;
+cout << "Press 'f' to cnovert this from Celsius to Fahrenheit" << endl;
+	
+//takes the unit for the conversion
+	char unit;
+	cin >> unit;
+
+switch (unit)
+{
+//converts inputted temperature to celsius
+case 'c':
+case 'C':
+{
+int anstempc = t - 32 * 0.56;
+cout << "The temperature from Fahrenheit to Celsius is: " << anstempc;
+break;
+}
+//converts inputted temperature to fahrenheit
+case 'f':
+case 'F':
+{
+int anstempf = t * 1.8; 
+cout << "The temperature from Celsius to Fahrenheit is: " << anstempf;
+break;
+}
+default:
+cout << "Invalid input";
+break;
+}
+*/
+	return 0;
+}
+
